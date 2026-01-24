6ea441bba02844aeba585876945553ce4d2ad3e9
moicben <moicben@email.com>
2026-01-24 20:43:09 +0800

    up

---
+++ .next/dev/server/pages/api/instances.js
@@ -0,0 +1,5 @@
+var R=require("../../chunks/[turbopack]_runtime.js")("server/pages/api/instances.js")
+R.c("server/chunks/node_modules_next_dist_6a63cc8e._.js")
+R.c("server/chunks/[root-of-the-server]__39ad551e._.js")
+R.m("[project]/node_modules/next/dist/esm/build/templates/pages-api.js { INNER_PAGE => \"[project]/pages/api/instances.js [api] (ecmascript)\" } [api] (ecmascript)")
+module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/pages-api.js { INNER_PAGE => \"[project]/pages/api/instances.js [api] (ecmascript)\" } [api] (ecmascript)").exports
---
+++ .next/dev/static/development/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/pages/index.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/api/config",
+    "/api/containers",
+    "/api/instances",
+    "/api/meetings"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
