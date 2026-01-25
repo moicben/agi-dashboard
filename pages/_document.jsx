13c2ddbb6bd92a5e8daf4c21b718a5e312fc5a7e
moicben <moicben@email.com>
2026-01-25 10:01:02 +0800

    up

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,16 +6,9 @@ export default function Document() {
             <Head>
                 <meta name="theme-color" content="#667eea" />
                 <link rel="shortcut icon" href="/favicon.png" />
-                <link rel="icon" type="image/png" href="/favicon.png" />
-                <link rel="apple-touch-icon" sizes="57x57" href="/favicon.png" />
-                <link rel="apple-touch-icon" sizes="60x60" href="/favicon.png" />
-                <link rel="apple-touch-icon" sizes="72x72" href="/favicon.png" />
-                <link rel="apple-touch-icon" sizes="76x76" href="/favicon.png" />
-                <link rel="apple-touch-icon" sizes="114x114" href="/favicon.png" />
-                <link rel="apple-touch-icon" sizes="120x120" href="/favicon.png" />
-                <link rel="apple-touch-icon" sizes="144x144" href="/favicon.png" />
-                <link rel="apple-touch-icon" sizes="152x152" href="/favicon.png" />
-                <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png" />
+                <link rel="icon" href="/favicon.png" />
+                <link rel="preload" as="image" href="/favicon.png" />
+
             </Head>
             <body>
                 <Main />
