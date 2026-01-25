9f535b87c2d5cdcd77150889e2a9d9250da79a89
moicben <moicben@email.com>
2026-01-25 10:08:24 +0800

    up

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,10 +5,11 @@ export default function Document() {
         <Html lang="fr">
             <Head>
                 <meta name="theme-color" content="#667eea" />
-                <link rel="icon" type="image/png" href="/favicon.png" />
-                <link rel="shortcut icon" type="image/png" href="/favicon.png" />
-                <link rel="apple-touch-icon" href="/favicon.png" />
-                <link rel="preload" as="image" href="/favicon.png" />
+                {/* Safari cache très agressivement les favicons :
+                   on "bust" l'URL et on déclare explicitement les tailles (le PNG est 800x800). */}
+                <link rel="icon" type="image/png" sizes="800x800" href="/favicon.png?v=dashboard-1" />
+                <link rel="shortcut icon" type="image/png" sizes="800x800" href="/favicon.png?v=dashboard-1" />
+                <link rel="apple-touch-icon" sizes="800x800" href="/favicon.png?v=dashboard-1" />
 
             </Head>
             <body>
